fd9532d2c7e4fdef9f07c4d8969b91c8a5081d06
Ga Young Kim <gayoung1@ualberta.ca>
2014-07-28 16:41:10 -0600

    copied the login from previous project

---
+++ CardiacRehab/CardiacRehab/Login.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CardiacRehab
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+            FocusManager.SetFocusedElement(loginscreen, usernameinput);
+        }
+
+        private void login_button_Click(object sender, RoutedEventArgs e)
+        {
+            String username = usernameinput.Text;
+            String password = passwordinput.Password;
+            int chosenIndex = indexinput.SelectedIndex;
+
+            // database connection and check login info
+        }
+
+        // need this function to close the hidden login form
+        private void MainWindowClosed(object sender, EventArgs e)
+        {
+            Console.WriteLine("Closing loginwindow");
+            this.Close();
+        }
+    }
+}
