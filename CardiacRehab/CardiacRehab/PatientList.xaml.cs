3e797397d3e7d0744b516ae5faaf6d3d5a2044f7
Ga Young Kim <gayoung1@ualberta.ca>
2014-08-08 11:58:55 -0600

    the UI for the patient waiting list is done

--- CardiacRehab/CardiacRehab/PatientList.xaml.cs
+++ CardiacRehab/CardiacRehab/PatientList.xaml.cs
@@ -19,9 +19,58 @@ namespace CardiacRehab
     /// </summary>
     public partial class PatientList : Window
     {
+        /// <summary>
+        /// This window needs to:
+        ///     - when loading: need to read the input from the select menu and list patients dynamically
+        ///     1) query call patient under the supervising doctor
+        ///     2) check the URL(../doctors/<dbID>/patients/<dbID>/) for all to see if they are online
+        ///         (then update the list to exclude the online patients)
+        ///     3) update the list accordingly
+        /// </summary>
+         
+
         public PatientList()
         {
             InitializeComponent();
+
+        }
+
+        private void max_patients_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // dynamically change the list of patients
+            // (need to check for the label content before to see if the patient has already connected when the
+            // doctor changed the number of patients)
+
+            int maxNum = max_patients.SelectedIndex;
+
+            // find number of connected patients
+            String[] connected = null;
+            int connected_index = 0;
+            for(int i = 1; i < 7; i++)
+            {
+                Label label = (Label)this.FindName("patient_status" + i.ToString());
+                String content = label.Content.ToString();
+
+                if(!content.Contains("Waiting for connection"))
+                {
+                    connected[connected_index] = "patient_status" + i.ToString();
+                    connected_index++;
+                }
+            }
+
+            if(connected.Length == maxNum)
+            {
+                MessageBox.Show("GOT EVERYONE!");
+            }
+            else if(connected.Length < maxNum)
+            {
+                int difference = maxNum - connected.Length;
+                MessageBox.Show("Missing " + difference.ToString() + " patients!");
+            }
+            else
+            {
+                MessageBox.Show("Something went wrong!");
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 CardiacRehab
 ============
+Checkmark icon from:
+http://www.iconsdb.com/green-icons/checkmark-icon.html
+
+All black and white icons in application from:
+http://www.flaticon.com/authors/freepik
