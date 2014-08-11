675dd8c61fd89ef690824034bfbd93c232b9666b
Ga Young Kim <gayoung1@ualberta.ca>
2014-08-11 12:30:46 -0600

    now the UI dynamically changes according to the select menu if no patients are connected

--- CardiacRehab/CardiacRehab/PatientList.xaml.cs
+++ CardiacRehab/CardiacRehab/PatientList.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -27,7 +28,10 @@ namespace CardiacRehab
         ///         (then update the list to exclude the online patients)
         ///     3) update the list accordingly
         /// </summary>
-         
+
+        int oldSelection = 5;
+        int newSelection = 0;
+
 
         public PatientList()
         {
@@ -41,7 +45,7 @@ namespace CardiacRehab
             // (need to check for the label content before to see if the patient has already connected when the
             // doctor changed the number of patients)
 
-            int maxNum = max_patients.SelectedIndex;
+            newSelection = max_patients.SelectedIndex;
 
             // find number of connected patients
             String[] connected = null;
@@ -68,13 +72,13 @@ namespace CardiacRehab
 
             if(connected != null)
             {
-                if (connected.Length == maxNum)
+                if (connected.Length == newSelection)
                 {
                     MessageBox.Show("GOT EVERYONE!");
                 }
-                else if (connected.Length < maxNum)
+                else if (connected.Length < newSelection)
                 {
-                    int difference = maxNum - connected.Length;
+                    int difference = newSelection - connected.Length;
                     MessageBox.Show("Missing " + difference.ToString() + " patients!");
                 }
                 else
@@ -85,6 +89,62 @@ namespace CardiacRehab
             else
             {
                 MessageBox.Show("No Patients are connected");
+                TogglePatientList(oldSelection, newSelection, "none");
+            }
+
+            oldSelection = newSelection;
+        }
+
+        private void TogglePatientList(int old, int selected, String connected)
+        {
+            Console.WriteLine("old: " + old.ToString());
+            Console.WriteLine("selected: " + selected.ToString());
+
+            if(connected == "none")
+            {
+                if(old > selected)
+                {
+                    int endNumber = selected + 1;
+                    int diff = old - selected;
+
+                    for (int i = 0; i < diff; i++)
+                    {
+                        endNumber++;
+
+                        Console.WriteLine("Looking for: patient_rec" + endNumber.ToString());
+
+                        Rectangle rec = (Rectangle)this.FindName("patient_rec" + endNumber.ToString().Trim());
+                        rec.Visibility = System.Windows.Visibility.Hidden;
+
+                        Label patientStatus = (Label)this.FindName("patient_status" + endNumber.ToString());
+                        patientStatus.Visibility = System.Windows.Visibility.Hidden;
+                    }
+                }
+                // increased maximum
+                else
+                {
+                    int endNumber = old + 1;
+                    int diff = selected - old;
+
+                    for (int i = 0; i < diff; i++)
+                    {
+                        endNumber++;
+
+                        Console.WriteLine("Looking for: patient_rec" + endNumber.ToString());
+
+                        Rectangle rec = (Rectangle)this.FindName("patient_rec" + endNumber.ToString().Trim());
+                        rec.Visibility = System.Windows.Visibility.Visible;
+
+                        Label patientStatus = (Label)this.FindName("patient_status" + endNumber.ToString());
+                        patientStatus.Visibility = System.Windows.Visibility.Visible;
+
+                    }
+                }
+                
+            }
+            else
+            {
+
             }
         }
     }
